816dd4e6fc620dae793d9a8f82deb00545ad6746
DhruvMahindru8104 <dhruv.mahindru08@gmail.com>
2025-07-29 18:08:06 +0530

    first commit

---
+++ src/lib/sanity.js
@@ -0,0 +1,6 @@
+import {createClient} from 'next-sanity';
+export const client = createClient({
+  projectId: "9fd6bban",
+  dataset: "production",
+    apiVersion: "2023-10-17",
+    useCdn: true,});
