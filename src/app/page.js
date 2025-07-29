eafd715d730b1d144a97e1d0f324f8e7035cccf6
DhruvMahindru8104 <dhruv.mahindru08@gmail.com>
2025-07-29 18:31:56 +0530

    third

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,9 @@ import { client } from '../lib/sanity'
 
 export default async function Home() {
     const query = `*[_type == "blog"] | order(_createdAt desc)`
-  const blogs = await client.fetch(query,{cache:'no-store'});
+  const blogs = await client.fetch(
+    query,
+    {cache:'no-store'});
   return (
     <div>
       <h1>All Blogs</h1>
@@ -15,6 +17,7 @@ export default async function Home() {
         <div key={blog._id}>
           <h2>{blog.title}</h2>
           <p>{blog.body}</p>
+          production
         </div>
       ))}
     </div>
